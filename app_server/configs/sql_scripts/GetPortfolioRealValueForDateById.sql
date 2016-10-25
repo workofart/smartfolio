c9ef263a2588869deedc7084d7dcd26c847e53ba
Ben Ling <belinghy@gmail.com>
2016-10-25 00:08:25 -0400

    Some optimization for calculating performance

--- app_server/configs/sql_scripts/GetPortfolioRealValueForDateById.sql
+++ app_server/configs/sql_scripts/GetPortfolioRealValueForDateById.sql
@@ -14,6 +14,7 @@ RETURNS TABLE (
 AS
 $$
 BEGIN
+	-- Get Position for each ticker
 	DROP TABLE IF EXISTS StockQuantity;
 	CREATE TEMP TABLE StockQuantity (
     	ticker VARCHAR(8),
@@ -21,17 +22,28 @@ BEGIN
     );
 
 	INSERT INTO StockQuantity (ticker, quantity)
-    SELECT
-        T.ticker,
-        Sum(T.quantity)
-    FROM portfolios AS P
-    INNER JOIN transactions AS T ON T.portfolioid = P.portfolioid
-    WHERE P.portfolioid = s_portfolioid
-      AND T.status = 1
+    SELECT DISTINCT ON (T.ticker)
+        T.ticker, T.position
+    FROM transactions AS T
+    WHERE T.portfolioid = s_portfolioid
       AND date(T.datetime) <= s_date
-    GROUP BY T.ticker;
+    ORDER BY T.ticker, T.transactionid DESC;
 
+	-- Get latest price for each ticker
+	DROP TABLE IF EXISTS PriceTable;
+	CREATE TEMP TABLE PriceTable (
+    	ticker VARCHAR(8),
+    	price NUMERIC
+    );
+    
+    INSERT INTO PriceTable (ticker, price)
+    SELECT DISTINCT ON (SD.ticker)
+    	SD.ticker, SD.close
+    FROM stock_daily AS SD
+    WHERE date(SD.datetime) <= s_date
+    ORDER BY SD.ticker, SD.insertid DESC;
 
+	-- Get Price * Position for each ticker
     DROP TABLE IF EXISTS OutputTable;
 	CREATE TEMP TABLE OutputTable (
         ticker VARCHAR(8),
@@ -41,15 +53,11 @@ BEGIN
     INSERT INTO OutputTable (ticker, value)
     SELECT
     	SQ.ticker,
-        SUM(SQ.quantity * SD.close)
+        SQ.quantity * PT.price
     FROM StockQuantity AS SQ
-    LEFT OUTER JOIN stock_daily AS SD ON SQ.ticker = SD.ticker
-    WHERE SD.datetime = ( SELECT MAX(datetime) 
-                            FROM stock_daily AS SD2 
-                           WHERE SD2.ticker = SD.ticker 
-                             AND date(SD2.datetime) <= s_date)
-    GROUP BY SQ.ticker;
-    
+    INNER JOIN PriceTable AS PT ON SQ.ticker = PT.ticker;
+
+	-- Add cash balance into output
     INSERT INTO OutputTable (ticker, value)
     SELECT
         T.ticker,
